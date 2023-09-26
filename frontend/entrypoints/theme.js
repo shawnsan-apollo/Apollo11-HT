637c94de42889c2d58a9eebbf7bc067b8b7b72d4
Shawnsan <santiagoshawn@gmail.com>
2023-09-26 14:13:34 -0400

    added service workers | Image snippet

--- frontend/entrypoints/theme.js
+++ frontend/entrypoints/theme.js
@@ -6,3 +6,11 @@ const summaries = document.querySelectorAll('[id^="Details-"] summary')
 
 revive(islands)
 initDisclosureWidgets(summaries)
+
+const modules = import.meta.glob(['../store/**/scripts/*.js'])
+
+for (const path in modules) {
+  if (!path.includes('core')) {
+    modules[path]() // this imports the module and runs any code at the top level of the module
+  }
+}
