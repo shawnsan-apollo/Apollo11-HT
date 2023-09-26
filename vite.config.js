0be700bfcd7526d274539ca1dc31f4d42387ee0c
Shawnsan <santiagoshawn@gmail.com>
2023-09-26 13:30:09 -0400

    reorganizing folder

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite'
 import shopify from 'vite-plugin-shopify'
+import globs from 'rollup-plugin-globlin'
+import cleanup from '@by-association-only/vite-plugin-shopify-clean'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [shopify()],
+  plugins: [
+    shopify(),
+    cleanup(),
+    globs({
+      globs: ['frontend/store/**/sections/*.liquid'],
+      dest: 'sections',
+      clean: false,
+    }),
+    globs({
+      globs: ['frontend/liquid/**/snippets/*.liquid'],
+      dest: 'snippets',
+      clean: false,
+    }),
+  ],
+  css: {
+    devSourcemap: true,
+  },
   build: {
+    emptyOutDir: false,
     rollupOptions: {
       output: {
         entryFileNames: '[name].[hash].min.js',
